0e1d6190939ee7b55650875513d25153133507e5
paolodeidda <paolodeidda.97@gmail.com>
2025-11-19 21:32:36 +0100

    automatic update

--- project_4/Skeleton_codes/ring/ring_sum.c
+++ project_4/Skeleton_codes/ring/ring_sum.c
@@ -10,7 +10,24 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // IMPLEMENT: Ring sum algorithm
-  int sum = 0; // initialize sum
+  int next = (rank + 1) % size;
+  int prev = (rank - 1 + size) % size;
+
+  int send_val = rank;
+  int sum = rank;
+
+  for (int step = 0; step < size - 1; ++step) {
+    int recv_val = 0;
+    MPI_Sendrecv(
+      &send_val, 1, MPI_INT, next, 0,
+      &recv_val, 1, MPI_INT, prev, 0, 
+      MPI_COMM_WORLD, MPI_STATUS_IGNORE
+    );
+    
+    sum += recv_val;
+    send_val = recv_val;
+  }  
+  
   printf("Process %i: Sum = %i\n", rank, sum);
 
   // Finalize MPI
---
+++ project_4/Skeleton_codes/ring/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ring
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=00:10:00
+#SBATCH --exclusive
+#SBATCH --output=ring_%j.out
+#SBATCH --error=ring_%j.err
+
+module load openmpi
+
+make clean
+make
+
+srun ./ring
