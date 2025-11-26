a7195ec6b600c582935fe7c9748298858ed02a81
paolodeidda <paolodeidda.97@gmail.com>
2025-11-26 15:29:45 +0100

    automatic update

--- project_5/Skeleton_codes/mini_app/strong_scaling_test.sh
+++ project_5/Skeleton_codes/mini_app/strong_scaling_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 #SBATCH --exclusive
 #SBATCH --output=strong_%j.out
 #SBATCH --error=strong_%j.err
@@ -13,11 +13,17 @@ module load gcc openmpi
 make clean
 make
 
-N=1024         
-NT=100
-TF=0.005
+NS=(64 128 256 512 1024)
 PROCS=(1 2 4 8 16)
 
-for P in "${PROCS[@]}"; do
-    srun --ntasks=$P ./main $N $NT $TF | tee -a strong_results.txt
+for N in "${NS[@]}"; do
+    echo "---------------------------" | tee -a strong_results.txt
+    echo "Grid size N = $N" | tee -a strong_results.txt
+
+    for P in "${PROCS[@]}"; do
+        echo "=== Running with $P processes ===" | tee -a strong_results.txt
+
+        srun --ntasks=$P ./main $N 100 0.005 | tee -a strong_results.txt
+    done
+    echo "" | tee -a strong_results.txt
 done
