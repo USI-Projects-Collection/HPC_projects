b80f21f3ba973efb77b13ece2bdda80f552e0ada
paolodeidda <paolodeidda.97@gmail.com>
2025-11-26 12:45:55 +0100

    automatic update

--- project_5/Skeleton_codes/mini_app/strong_scaling_test.sh
+++ project_5/Skeleton_codes/mini_app/strong_scaling_test.sh
@@ -19,9 +19,5 @@ TF=0.005
 PROCS=(1 2 4 8 16)
 
 for P in "${PROCS[@]}"; do
-    echo "====================================" | tee -a strong_results.txt
-    echo "Running with P = $P" | tee -a strong_results.txt
-    echo "" | tee -a strong_results.txt
-
-    srun --ntasks=$P ./mini $N $NT $TF | tee -a strong_results.txt
+    srun --ntasks=$P ./mini_app $N $NT $TF | tee -a strong_results.txt
 done
