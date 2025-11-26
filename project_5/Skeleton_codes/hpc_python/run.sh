f38dbfbff9ad507de55d4fde02ca5d60bb835dfd
paolodeidda <paolodeidda.97@gmail.com>
2025-11-26 21:34:04 +0100

    automatic update

--- project_5/Skeleton_codes/hpc_python/run.sh
+++ project_5/Skeleton_codes/hpc_python/run.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 #SBATCH --nodes=2
 #SBATCH --ntasks=8
 #SBATCH --ntasks-per-node=4
+
+#SBATCH --time=00:10:00
+#SBATCH --exclusive
+#SBATCH --output=python_%j.out
 
 module load python mpi4py gcc openmpi
 
