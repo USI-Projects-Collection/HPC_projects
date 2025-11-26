5c105e4e531340f17945220e63c6a2aba342d5f5
paolodeidda <paolodeidda.97@gmail.com>
2025-11-26 12:44:46 +0100

    automatic update

---
+++ project_5/Skeleton_codes/mini_app/strong_scaling/strong_scaling_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=pde_strong
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --exclusive
+#SBATCH --output=strong_%j.out
+#SBATCH --error=strong_%j.err
+
+module load gcc openmpi
+
+make clean
+make
+
+N=1024         
+NT=100
+TF=0.005
+PROCS=(1 2 4 8 16)
+
+for P in "${PROCS[@]}"; do
+    echo "====================================" | tee -a strong_results.txt
+    echo "Running with P = $P" | tee -a strong_results.txt
+    echo "" | tee -a strong_results.txt
+
+    srun --ntasks=$P ./mini $N $NT $TF | tee -a strong_results.txt
+done
