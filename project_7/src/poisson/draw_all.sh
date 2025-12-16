09655bf863397ac938cca04c53dc16316c289c1c
Paolo Deidda <69177574+DePoli97@users.noreply.github.com>
2025-12-16 19:14:35 +0100

    manual update

--- project_7/src/poisson/draw_all.sh
+++ project_7/src/poisson/draw_all.sh
@@ -9,10 +9,11 @@ python3 ../draw.py solution_sp_cg
 cd ../test_perf
 python3 ../draw.py solution_petsc
 python3 ../draw.py solution_sp_dir
+python3 ../draw.py solution_dn_dir
 python3 ../draw.py solution_sp_cg
+
 
 cd ../test_scale
 python3 ../draw.py solution_petsc
 
 cd ..
-echo "All plots generated!"
