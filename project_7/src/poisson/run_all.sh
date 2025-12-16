bb274b382563356128aca03a8515aaa88dda3cfc
Paolo Deidda <69177574+DePoli97@users.noreply.github.com>
2025-12-16 17:29:46 +0100

    manual update

--- project_7/src/poisson/run_all.sh
+++ project_7/src/poisson/run_all.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "Submitting validation test..."
 cd ./test_val
 sbatch test.sh
 
-echo "Submitting performance test..."
 cd ../test_perf
 sbatch test.sh
 
-echo "Submitting scaling test..."
 cd ../test_scale
 sbatch test.sh
 
 cd ..
-echo "All jobs submitted. Use 'squeue' to check status."
