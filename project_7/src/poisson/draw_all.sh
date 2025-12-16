d39c1396002e69b6d2c184968b5d900afa65f1d0
Paolo Deidda <69177574+DePoli97@users.noreply.github.com>
2025-12-16 21:44:24 +0100

    manual update

--- project_7/src/poisson/draw_all.sh
+++ project_7/src/poisson/draw_all.sh
@@ -1,25 +1,17 @@
-# #!/bin/bash
+#!/bin/bash
 
-# cd ./test_val
-# python3 ../draw.py solution_petsc
-# python3 ../draw.py solution_sp_dir
-# python3 ../draw.py solution_dn_dir
-# python3 ../draw.py solution_sp_cg
-# echo "done test_val"
-
-# cd ../test_perf
-# python3 ../draw.py solution_petsc
-# python3 ../draw.py solution_sp_dir
-# python3 ../draw.py solution_dn_dir
-# python3 ../draw.py solution_sp_cg
-# echo "done test_perf"
-
-
-cd ../test_scale
+cd test_val
 python3 ../draw.py solution_petsc
 python3 ../draw.py solution_sp_dir
 python3 ../draw.py solution_dn_dir
 python3 ../draw.py solution_sp_cg
-echo "done test_scale"
+
+cd ../test_perf
+python3 ../draw.py solution_petsc
+python3 ../draw.py solution_sp_dir
+python3 ../draw.py solution_sp_cg
+
+cd ../test_scale
+python3 ../draw.py solution_petsc
 
 cd ..
